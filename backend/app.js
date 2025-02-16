6d84fc5b322da5372b8c0c1af90277672addc602
Houssam Eddine HAMOUICH <itshoussameddine@gmail.com>
2025-02-16 18:27:26 +0100

    add port

--- backend/app.js
+++ backend/app.js
@@ -21,4 +21,4 @@ mongoose.connect(`${process.env.MONGO_URI}/crud-app`, {
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log(`Server running on port ${PORT}`));
