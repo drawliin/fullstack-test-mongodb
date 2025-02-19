d6f0493df6259d9e8521da2b377148aa41540c9c
Houssam Eddine HAMOUICH <itshoussameddine@gmail.com>
2025-02-20 00:37:21 +0100

    Add API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,19 @@
 // frontend/src/services/api.js
 import axios from "axios";
-const port = process.env.PORT || 4002;
-const API_URL = `http://localhost:${port}/api/users`;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4002/api';
+
 
 export const fetchUsers = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(`${API_URL}/users`);
   return response.data;
 };
 
 export const createUser = async (user) => {
-  const response = await axios.post(API_URL, user);
+  const response = await axios.post(`${API_URL}/users`, user);
   return response.data;
 };
 
 export const deleteUser = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
+  const response = await axios.delete(`${API_URL}/users/${id}`);
   return response.data;
 };
